9f184cd35680f0aafd581bc00a948c216ac1dbce
uglykoyote <uglykoyote@gmail.com>
2022-03-01 12:42:56 +0100

    Add a lot of changes in docker-entrypoint.sh

--- Python/aux_scripts/new_persons_generator.py
+++ Python/aux_scripts/new_persons_generator.py
@@ -5,6 +5,7 @@ from mimesis.builtins import RussiaSpecProvider
 from mimesis import Address
 import os
 import sys
+import psycopg2
 
 
 def generate_bulk(count):
@@ -19,6 +20,66 @@ def generate_bulk(count):
     })
     res = schema.create(iterations=count)
     return(res)
+
+
+def get_creds():
+    if os.getenv('DB_USER_NAME'):
+        if os.getenv('DB_USER_PASS'):
+            if os.getenv('DB_USER_DB'):
+                if os.getenv('DB_USER_PG_HOST'):
+                    if os.getenv('DB_USER_PG_PORT'):
+                        dbport = os.getenv('DB_USER_PG_PORT')
+                    else:
+                        dbport = "5432"
+                    dbhost = os.getenv('DB_USER_PG_HOST')
+                dbname = os.getenv('DB_USER_DB')
+            dbpass = os.getenv('DB_USER_PASS')
+        dbuser = os.getenv('DB_USER_NAME')
+    else:
+        print('Some env varibale is not set or undefined. Script aborted', file = sys.stdout))
+        raise SystemExit(1)
+    return(dbname, dbuser, dbpass, dbhost, dbport)
+
+
+def insert(persons):
+    dbname, dbuser, dbpass, dbhost, dbport = get_creds()
+    # Connect to your postgres DB
+    try:
+        conn = psycopg2.connect(
+            host=dbhost,
+            database=dbname,
+            user=dbuser,
+            password=dbpass,
+            port=dbport
+        )
+    except:
+        print('Unable to connect to db server. Exiting', file = sys.stdout))
+        raise SystemExit(1)
+    # conn.autocommit = True
+    # print("Cursor opening. Sent to stdout", file = sys.stdout)
+    cursor = conn.cursor()
+    i = 0
+    persons_item_count = len(persons)
+    # print("Get pesron count in array. Sent to stdout", file = sys.stdout)
+    while i < persons_item_count:
+    # for person in load_json(persons):
+      # Open a cursor to perform database operations
+      # print("Read %s item from array. Sent to stdout" % i, file = sys.stdout)
+      person = json.loads(persons[i])
+      cursor.execute("INSERT INTO person (fio, phone, age, city, addr, inn) VALUES(%s, %s, %s, %s, %s, %s)", (person['fio'], person['phone'], person['age'], person['city'], person['address'], person['inn']))
+      # conn.commit()
+      # print("Cursor executed. Sent to stdout", file = sys.stdout)
+      i += 1
+    persons = {}
+    # print("Array is set to null. Sent to stdout", file = sys.stdout)
+    conn.commit()
+    # print("Made commit. Sent to stdout", file = sys.stdout)
+    cursor.close()
+    # print("Cursor closed. Sent to stdout", file = sys.stdout)
+    conn.close()
+    # print("Connection closed. Sent to stdout", file = sys.stdout)
+    gc.collect()
+    # print("Garbage collector executed. Sent to stdout", file = sys.stdout)
 
 
 def read_env():
@@ -36,17 +97,17 @@ def read_env():
         try:
             PERSON_COUNT = int(os.getenv('PERSON_COUNT'))
         except:
-            print('PERSON_COUNT is set but value is not number. Used default value - 10')
+            print('PERSON_COUNT is set but value is not number. Used default value - 10', file = sys.stdout))
             PERSON_COUNT = 10
     else:
-        print('PERSON_COUNT not found as env varibale. Used default value - 10')
+        print('PERSON_COUNT not found as env varibale. Used default value - 10', file = sys.stdout))
         PERSON_COUNT = 10
     return(SEND_TO_CONSOLE, CYCLIAL_MODE, PERSON_COUNT)
 
 
 def actions(SEND_TO_CONSOLE, CYCLIAL_MODE, persons):
     if CYCLIAL_MODE == "True":
-        print('Enable cyclic mode')
+        print('Enable cyclic mode', file = sys.stdout))
         while True:
             if SEND_TO_CONSOLE == "False":
                 insert(persons)
@@ -64,7 +125,6 @@ def actions(SEND_TO_CONSOLE, CYCLIAL_MODE, persons):
 
 def main():
     SEND_TO_CONSOLE, CYCLIAL_MODE, PERSON_COUNT = read_env()
-    print('SEND_TO_CONSOLE:{} CYCLIAL_MODE:{} PERSON_COUNT{}'.format(SEND_TO_CONSOLE, CYCLIAL_MODE, PERSON_COUNT))
     persons = generate_bulk(PERSON_COUNT)
     actions(SEND_TO_CONSOLE, CYCLIAL_MODE, persons)
 

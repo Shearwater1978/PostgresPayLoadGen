80861e412b8e54bc70096cad43875c07ee714af3
uglykoyote <uglykoyote@gmail.com>
2022-03-01 13:22:24 +0100

    Fixed another issues in scripts

--- Python/aux_scripts/docker-entrypoint.sh
+++ Python/aux_scripts/docker-entrypoint.sh
@@ -13,16 +13,16 @@ echo "DB_USER_PG_PORT: $DB_USER_PG_PORT"
 echo "SEND_TO_CONSOLE: $SEND_TO_CONSOLE"
 
 
-if [ "$SEND_TO_CONSOLE" == 'False' ]; then
+if [[ "$SEND_TO_CONSOLE" == 'False' ]]; then
    echo "Initial check DB connection started..."
    RES=`python check_conn.py`
    echo "${RES}"
 fi
 
-if [ "$SEND_TO_CONSOLE" == 'True' ]; then
+if [[ "$SEND_TO_CONSOLE" == 'True' ]]; then
    exec python new_persons_generator.py
 else
-   if [ "$RES" != 'Connection to PG is active.' ]; then
+   if [[ "$RES" != 'Connection to PG is active.' ]]; then
       echo "DB server is not available"
       echo "Termination..."
       exit 1

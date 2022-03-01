d775514525f5c6aaeb4f1873baa6196500ebb081
uglykoyote <uglykoyote@gmail.com>
2022-03-01 13:22:24 +0100

    Fixed another issues in scripts

--- Python/aux_scripts/new_persons_generator.py
+++ Python/aux_scripts/new_persons_generator.py
@@ -36,7 +36,7 @@ def get_creds():
             dbpass = os.getenv('DB_USER_PASS')
         dbuser = os.getenv('DB_USER_NAME')
     else:
-        print('Some env varibale is not set or undefined. Script aborted', file = sys.stdout))
+        print('Some env varibale is not set or undefined. Script aborted', file = sys.stdout)
         raise SystemExit(1)
     return(dbname, dbuser, dbpass, dbhost, dbport)
 
@@ -53,14 +53,12 @@ def insert(persons):
             port=dbport
         )
     except:
-        print('Unable to connect to db server. Exiting', file = sys.stdout))
+        print('Unable to connect to db server. Exiting', file = sys.stdout)
         raise SystemExit(1)
     # conn.autocommit = True
-    # print("Cursor opening. Sent to stdout", file = sys.stdout)
     cursor = conn.cursor()
     i = 0
     persons_item_count = len(persons)
-    # print("Get pesron count in array. Sent to stdout", file = sys.stdout)
     while i < persons_item_count:
     # for person in load_json(persons):
       # Open a cursor to perform database operations
@@ -68,18 +66,12 @@ def insert(persons):
       person = json.loads(persons[i])
       cursor.execute("INSERT INTO person (fio, phone, age, city, addr, inn) VALUES(%s, %s, %s, %s, %s, %s)", (person['fio'], person['phone'], person['age'], person['city'], person['address'], person['inn']))
       # conn.commit()
-      # print("Cursor executed. Sent to stdout", file = sys.stdout)
       i += 1
     persons = {}
-    # print("Array is set to null. Sent to stdout", file = sys.stdout)
     conn.commit()
-    # print("Made commit. Sent to stdout", file = sys.stdout)
     cursor.close()
-    # print("Cursor closed. Sent to stdout", file = sys.stdout)
     conn.close()
-    # print("Connection closed. Sent to stdout", file = sys.stdout)
     gc.collect()
-    # print("Garbage collector executed. Sent to stdout", file = sys.stdout)
 
 
 def read_env():
@@ -97,17 +89,17 @@ def read_env():
         try:
             PERSON_COUNT = int(os.getenv('PERSON_COUNT'))
         except:
-            print('PERSON_COUNT is set but value is not number. Used default value - 10', file = sys.stdout))
+            print('PERSON_COUNT is set but value is not number. Used default value - 10', file = sys.stdout)
             PERSON_COUNT = 10
     else:
-        print('PERSON_COUNT not found as env varibale. Used default value - 10', file = sys.stdout))
+        print('PERSON_COUNT not found as env varibale. Used default value - 10', file = sys.stdout)
         PERSON_COUNT = 10
     return(SEND_TO_CONSOLE, CYCLIAL_MODE, PERSON_COUNT)
 
 
 def actions(SEND_TO_CONSOLE, CYCLIAL_MODE, persons):
     if CYCLIAL_MODE == "True":
-        print('Enable cyclic mode', file = sys.stdout))
+        print('Enable cyclic mode', file = sys.stdout)
         while True:
             if SEND_TO_CONSOLE == "False":
                 insert(persons)

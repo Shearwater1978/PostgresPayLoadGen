b072e0e0bdad3d27a74c061474d0f4f43e0b1c6d
Uglykoyote <uglykoyote@yandex.ru>
2022-03-06 16:47:28 +0100

    Fixed a lot of issues. Add new person generator [ci skip]

--- Python/aux_scripts/docker-entrypoint.sh
+++ Python/aux_scripts/docker-entrypoint.sh
@@ -2,16 +2,19 @@
 
 set -e
 
-echo "Check env variables"
-echo "CYCLIAL_MODE: $CYCLIAL_MODE"
-echo "PERSON_COUNT: $PERSON_COUNT"
-echo "DB_USER_NAME: $DB_USER_NAME"
-echo "DB_USER_PASS: $DB_USER_PASS"
-echo "DB_USER_DB: $DB_USER_DB"
-echo "DB_USER_PG_HOST: $DB_USER_PG_HOST"
-echo "DB_USER_PG_PORT: $DB_USER_PG_PORT"
-echo "SEND_TO_CONSOLE: $SEND_TO_CONSOLE"
-echo "RANDOM_FACTOR: $RANDOM_FACTOR"
+if [[ "$DEBUG_ON" == "Yes" ]]; then
+   echo "Check env variables"
+   echo "CYCLIAL_MODE: $CYCLIAL_MODE"
+   echo "PERSON_COUNT: $PERSON_COUNT"
+   echo "DB_USER_NAME: $DB_USER_NAME"
+   echo "DB_USER_PASS: $DB_USER_PASS"
+   echo "DB_USER_DB: $DB_USER_DB"
+   echo "DB_USER_PG_HOST: $DB_USER_PG_HOST"
+   echo "DB_USER_PG_PORT: $DB_USER_PG_PORT"
+   echo "SEND_TO_CONSOLE: $SEND_TO_CONSOLE"
+   echo "RANDOM_FACTOR: $RANDOM_FACTOR"
+   echo "NAME_OF_GENERATOR: $NAME_OF_GENERATOR"
+fi
 
 if [[ "$SEND_TO_CONSOLE" == "False" ]]; then
    echo "Initial check DB connection started..."
@@ -20,7 +23,7 @@ if [[ "$SEND_TO_CONSOLE" == "False" ]]; then
 fi
 
 if [[ "$SEND_TO_CONSOLE" == "True" ]]; then
-   exec python mimesis_persons_generator.py
+   exec python new_persons_generator.py
 else
    if [[ "$RES" != "Connection to PG is active." ]]; then
       echo "DB server is not available"
@@ -31,11 +34,11 @@ else
          while :
          do
             echo "Run send payload in cyclial mode..."
-            python mimesis_persons_generator.py
+            python new_persons_generator.py
          done
       else
          echo "Send payload at once time..."
-         python mimesis_persons_generator.py
+         python new_persons_generator.py
       fi
    fi
 fi
